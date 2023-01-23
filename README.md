8ac5bcaffa2db9d56451c25af844e25c59847827
Hafiz Haseeb Ahmad Butt <hbutt.bscs19seecs@seecs.edu.pk>
2023-01-24 02:47:37 +0500

    Header Started

---
+++ README.md
@@ -0,0 +1,2 @@
+## Mobile Store In Laravel
+This is a simple mobile store application built with Laravel 9.19 and Tailwind CSS 3.2.4.
---
+++ resources/views/components/header-component.blade.php
@@ -0,0 +1,14 @@
+<div>
+{{--    Applying Tailwind CSS Grid classes--}}
+    <div class="w-full h-max bg-blue-300 grid grid-cols-3">
+        <div class="w-full h-10 bg-black">
+
+        </div>
+        <div class="w-full h-10 bg-green-300">
+
+        </div>
+        <div class="w-full h-10 bg-gray-400">
+
+        </div>
+    </div>
+</div>
